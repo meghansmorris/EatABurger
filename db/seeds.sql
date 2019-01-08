b45189916e4378cd254199fb88fb3b906311f9e8
itsmegh <42684917+itsmegh@users.noreply.github.com>
2019-01-08 12:11:00 -0700

    finalizing the file structure

---
+++ db/seeds.sql
@@ -0,0 +1,7 @@
+
+INSERT INTO burgers (burger_name, devoured) VALUES ('Cheeseburger', false);
+INSERT INTO burgers (burger_name, devoured) VALUES ('Veggie Burger', false);
+INSERT INTO burgers (burger_name, devoured) VALUES ('Portabello Burger', false);
+INSERT INTO burgers (burger_name, devoured) VALUES ('Basic Burger', false);
+
+SELECT * FROM burgers;
